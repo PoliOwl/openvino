a38801192c498b20fc1d887e1c51ff750df11242
Polina <polina.brzezinskaya@intel.com>
2020-07-24 18:07:29 +0300

    startedworking on subgraph

--- inference-engine/tools/vpu/vpu_graph_editor/main.cpp
+++ inference-engine/tools/vpu/vpu_graph_editor/main.cpp
@@ -4,12 +4,15 @@
 #include "net_pass.h"
 #include <fstream>
 #include <set>
+#include <vector>
 #include <string>
 #include <cstdlib>
+#include <map>
+#include <ngraph/opsets/opset3.hpp>
 #include "cnn_network_ngraph_impl.hpp"
 
 
-#define DEBUG 0
+#define DEBUG 3
 
 
 //struct for file paths used to get needed data
@@ -48,31 +51,33 @@ void readNames(std::set<std::string>& NamesCont, const std::string& filePath) {
     std::string s; 
     while (std::getline(f, s)) {
        if(s.size() > 0){
-           NamesCont.insert(s);
+           NamesCont.insert(std::move(s));
        }
     }
 }
 
-
-//reads network
-InferenceEngine::details::CNNNetworkNGraphImpl* getNetwork(const filePaths& file) {
-    InferenceEngine::CNNNetwork network;
-    if (file.weightsPath != "NONE") {
-        network = InferenceEngine::Core().ReadNetwork(file.IRpath, file.weightsPath);
+template <typename T>
+std::shared_ptr<T> find(std::vector<std::shared_ptr<T>>& vector,const std::string& node) { 
+    for(auto el : vector) {
+        if(el->get_friendly_name() == node) {
+            return el;
+        }
     }
-    else {
-        network = InferenceEngine::Core().ReadNetwork(file.IRpath);
-    }
-    InferenceEngine::ICNNNetwork::Ptr ptr = static_cast<InferenceEngine::ICNNNetwork::Ptr>(network);
-   
-    auto ngraphNetwork = dynamic_cast<InferenceEngine::details::CNNNetworkNGraphImpl*>(ptr.get());
-    return ngraphNetwork;
+    return std::shared_ptr<T>(nullptr);
 }
+
+
 
 int main(int argc, char* argv[]) {
     filePaths file(argc, argv);
-    std::set<std::string> names;
-    readNames(names, file.namePath);
+    std::set<std::string> out;
+    readNames(out, file.namePath);
+    #if DEBUG == 2
+    for(auto name: input) {
+        std::cout << name<<'\n';
+    }
+    std::cout << "___________________________";
+    #endif
     InferenceEngine::CNNNetwork network;
     if (file.weightsPath != "NONE") {
         network = InferenceEngine::Core().ReadNetwork(file.IRpath, file.weightsPath);
@@ -88,21 +93,100 @@ int main(int argc, char* argv[]) {
     }
     auto nGraphFunc = ngraphNetwork->getFunction();
     auto ops = nGraphFunc->get_ordered_ops();
-    std::fstream f;
-    std::set<std::string> check = names;
+    ngraph::ResultVector results;
+    std::map<std::string, std::shared_ptr<ngraph::Node>> nodes;
+    ngraph::ParameterVector parameters;
     for (auto op : ops) {
-        check.erase(op->get_friendly_name());
-        if (check.empty()) {
-            break;
+        if(out.erase(op->get_friendly_name()) > 0) {
+            if(!(op->is_parameter())) {
+                ngraph::OutputVector args;
+                for(auto& element : op->input_values()) {
+                    auto&& el = element.get_node_shared_ptr(); 
+                    auto&& name = el->get_friendly_name();
+                    auto subNode = nodes.find(name);
+                    if(subNode != nodes.end()) {
+                        args.push_back(subNode->second->output(element.get_index()));
+                       // std::cout<<subNode->second->output(element.get_index())<<'\n'<<subNode->second<<'\n';
+                        continue;
+                    }
+                    auto&& existing_parameter = find(parameters, name);
+                    if(existing_parameter.get() != nullptr) {
+                        //std::cout<<"Here: "<<name<<'\t'<<op->get_friendly_name()<<'\n';
+                        args.push_back(existing_parameter->output(element.get_index()));
+                        continue;
+                    }
+                    const auto& stubParameter = std::make_shared<ngraph::opset3::Parameter>(element.get_element_type(), element.get_partial_shape());
+                    stubParameter->set_friendly_name(name);
+                    parameters.push_back(stubParameter);
+                    args.push_back(stubParameter);
+                }
+                auto&& new_op = op->copy_with_new_inputs(args);
+                new_op->set_friendly_name(op->get_friendly_name());
+                nodes[op->get_friendly_name()] = new_op;
+            }
+            else {
+                const auto& stubParameter = std::make_shared<ngraph::opset3::Parameter>(op->get_element_type(), op->get_shape());
+                stubParameter->set_friendly_name(op->get_friendly_name());
+                parameters.push_back(stubParameter);
+            }
+            for(size_t i = 0; i < op->get_output_size();++i) {
+                auto set = op->get_output_target_inputs(i); 
+                for (auto sel : set) {
+                    auto&& el = sel.get_node();
+                    if(out.find(el->get_friendly_name()) == out.end()) {
+                        auto res = std::make_shared<ngraph::opset3::Result>(op);
+                        res->set_friendly_name(el->get_friendly_name());
+                        results.push_back(res);
+
+                    }
+                }
+            }
+        }
+        if (out.empty()) {
+           break;
         }
     }
-    if (!check.empty()) {
-        for (auto name : check) {
+    if (!out.empty()) {
+        for (auto name : out) {
             std::cout << name <<std::endl;
         }
         std::cout << "wasn't found. Stop"<< std::endl;
         return 0;
     }
+    #if DEBUG == 3
+    std::cout<<"\ninput:\n";
+    for(auto el : nodes) {
+        std::cout << "\t" << el.second->get_friendly_name() << "\t" <<el.second.get()<<'\n';
+        for(auto par_c : el.second->input_values()) {
+            auto par = par_c.as_single_output_node(); 
+            std::cout<<"\t\t"<<par->get_friendly_name()<<"\t"<<par.get()<<"\n\n";
+        }
+    }
+    std::cout<<"\nparameters:\n";
+    for(auto par : parameters) {
+        std::cout<<"\t"<<par->get_friendly_name()<<'\t'<<par.get()<<"\n";
+        for(auto par_c : par->input_values()) {
+            auto par_n = par_c.as_single_output_node(); 
+            std::cout<<"\t\t"<<par_n->get_friendly_name()<<"\t"<<par_n.get()<<"\n\n";
+        }
+    }
+    std::cout<<"\nresults:\n";
+    for(auto res : results){
+        std::cout<<"\t"<<res->get_friendly_name()<<'\t'<<res.get()<<"\n";
+        for(auto par_c : res->input_values()) {
+            auto par = par_c.as_single_output_node(); 
+            std::cout<<"\t\t"<<par->get_friendly_name()<<"\t"<<par.get()<<"\n\n";
+        }
+    }
+    #endif
     std::cout << "All names are valid" << std::endl;
+    ngraph::Function subgraphFunc(results, parameters, "Subgraph");
+    std::cout<<"Subgrap created"<<std::endl;
+    #if DEBUG == 3
+    auto s = subgraphFunc.get_ordered_ops();
+    for(auto op : s) {
+        std::cout<<op->get_friendly_name()<<'\n';
+    }
+    #endif
     return 0;
 }
